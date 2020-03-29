36170dd0032fea611a202d0f488c5d490f323ef4
t-daiki96 <t.daiki96@gmail.com>
2020-03-29 10:11:52 +0900

    state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import PropsTypes from 'prop-types'
+import React, { useState } from 'react'
 
 export default () => {
-  const users = [
-    { name: 'Taro', age: 10 },
-    { name: 'Hanako', age: 20 },
-    { name: 'Yu', age: 3 }
-  ]
+  return (<Counter />)
+}
+
+const Counter = () => {
+  let [counter, setCounter] = useState([0])
   return (
-    <div>
-      {users.map((user, index) => {
-        return <User name={user.name} age={user.age} key={index} />
-      })}
-    </div>
+    <React.Fragment>
+      <button onClick={() => { setCounter(++counter) }}>plus</button>
+      <button onClick={() => { setCounter(--counter) }}>minus</button>
+      {counter}
+    </React.Fragment>
   )
-}
-
-const User = props => {
-  return <div>I am {props.name}, and {props.age} years old</div>
-}
-
-User.propTypes = {
-  name: PropsTypes.string,
-  age: PropsTypes.number.isRequired
 }
